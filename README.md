117a960c634d0fd431c7ab361371570442c86b16
jeswr <jesse.wright@anu.edu.au>
2021-02-15 15:58:55 +1100

    WIP: Set up library configs, begin migrating from jeswr/on2ts

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# template-typescript
-Template repo for my Typescript projects
+# @on2ts/jsdoc-generation
+
+Generates JSDoc annotations based on ontology file declarations.
+
+[![GitHub license](https://img.shields.io/github/license/on2ts/jsdoc-generation.svg)](https://github.com/on2ts/jsdoc-generation/blob/master/LICENSE)
+[![npm version](https://img.shields.io/npm/v/@on2ts/jsdoc-generation.svg)](https://www.npmjs.com/package/@on2ts/jsdoc-generation)
+[![build](https://img.shields.io/github/workflow/status/on2ts/jsdoc-generation/Node.js%20CI)](https://github.com/on2ts/jsdoc-generation/tree/main/)
+[![Dependabot](https://badgen.net/badge/Dependabot/enabled/green?icon=dependabot)](https://dependabot.com/)
+[![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
+
+## Usage
+
+TODO: SHOW USAGE
+
+## License
+©2021–present
+[Jesse Wright](https://github.com/jeswr),
+[MIT License](https://github.com/jeswr/useState/blob/master/LICENSE).
--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,27 @@
-export default {};
+import type { ActorInitSparql } from '@comunica/actor-init-sparql';
+
+/**
+ * Tests if a property is an instance of owl:AnnotationProperty
+ * @param property The rdf property to test
+ * @param queryEngine A sparql query engine
+ */
+export async function IsAnnotationProperty(property: any, queryEngine: ActorInitSparql): Promise<boolean> {
+  const result = await queryEngine.query(`ASK { <${await property}> a <http://www.w3.org/2002/07/owl#AnnotationProperty> }`);
+  if (result.type !== 'boolean') {
+    throw new Error('Boolean result expected from ASK query')
+  }
+  return result.booleanResult;
+}
+
+/**
+ * Generates a typescript JSDoc node describing the particular subject
+ * @param subject LDflex subject entity (including iteration handlers)
+ * @param queryEngine Sparql query engine that conforms the the ActorInitSparql API
+ */
+export async function JSDocGenerator(subject: any, queryEngine: ActorInitSparql) {
+  // TODO [FUTURE]: REMOVE ANY
+  for await (const property of subject.properties.filter((property: any) => IsAnnotationProperty(property, queryEngine))) {
+
+  };
+  
+}
