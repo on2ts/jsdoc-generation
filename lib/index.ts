448c25ec3789dcd080932238e1aee74c44042f00
jeswr <jesse.wright@anu.edu.au>
2021-02-16 01:23:29 +1100

    WIP: Cleaning

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,6 @@
 import type { ActorInitSparql } from '@comunica/actor-init-sparql';
+import { ts } from 'ts-morph';
+import { TagData } from '../types';
 
 /**
  * Tests if a property is an instance of owl:AnnotationProperty
@@ -13,15 +15,56 @@ export async function IsAnnotationProperty(property: any, queryEngine: ActorInit
   return result.booleanResult;
 }
 
+// const descriptionPriority: string[] = [
+//   dcterms.description,
+//   rdfs.comment,
+//   skos.note,
+//   rdfs.label,
+//   dcterms.title
+// ]
+
 /**
  * Generates a typescript JSDoc node describing the particular subject
  * @param subject LDflex subject entity (including iteration handlers)
- * @param queryEngine Sparql query engine that conforms the the ActorInitSparql API
+ * @param queryEngine SPARQL query engine that conforms the the ActorInitSparql API.
+ * We assume that the engine is reasoned over the ontology and all imports
+ * @param descriptionPriority The priority with which a given property is
+ * to be used as the primary comment.
  */
-export async function JSDocGenerator(subject: any, queryEngine: ActorInitSparql) {
-  // TODO [FUTURE]: REMOVE ANY
-  for await (const property of subject.properties.filter((property: any) => IsAnnotationProperty(property, queryEngine))) {
+export async function JSDocGenerator(subject: any, queryEngine: ActorInitSparql, descriptionPriority: string[] = []) { 
+  /**
+   * All annotation properties assosiated to the subject, sorted in alphabetical order
+   */
+  const properties: any[] = await subject.properties.filter((property: any) => IsAnnotationProperty(property, queryEngine))
+    .map((property: any) => property)
+    .sort();
 
-  };
-  
+  /**
+   * Data for the JSDoc tags
+   */
+  const tagsData: TagData[] = await Promise.all(properties.map(
+    async property => ({
+      tag: `${await property.fragment}`,
+      property: `${await property}`,
+      comment: `${await subject[`${await property}`]}`
+    })
+  ))
+
+  /**
+   * The property to be used for the main comment. If there are no properties in the
+   * description priority list, then the longest annotation is used instead.
+   */
+  const property = descriptionPriority.find(property => properties.includes(property)) ?? 
+    tagsData.reduce(
+      (t: TagData | undefined, a: TagData) => t?.comment.length ?? 0 < a.comment.length ? a : t,
+      undefined
+    )?.property;
+
+  const templateTags = tagsData.filter(tag => tag.property !== property)
+    .map(({ tag, comment }: TagData) => ts.factory.createJSDocTemplateTag(
+      ts.factory.createIdentifier(tag), undefined, [], comment,
+    ))
+
+  const comment = tagsData.find(tag => tag.property === property)?.comment 
+  return ts.factory.createJSDocComment(comment, templateTags)
 }
---
+++ types/tag-data.d.ts
@@ -0,0 +1,5 @@
+export interface TagData {
+  tag: string;
+  property: string;
+  comment: string;
+}
